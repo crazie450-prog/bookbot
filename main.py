302f38d133fefd1b06c33fd7f67f7418b74e5503
crazie450-prog <crazie450@gmail.com>
2025-10-24 22:42:43 -0700

    added proper formatting for sort_char_count output and added frankenstein_filepath variable

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 # Import Functions from stats module
+import sys
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_char_count
 
 # Define Filepaths
-frankenstein_filepath = "books/frankenstein.txt"
+if len(sys.argv) == 2:
+    frankenstein_filepath = sys.argv [1]
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-
     
 def main():
     char_count = get_char_count(get_book_text(frankenstein_filepath))
-    
+    word_count = get_num_words(get_book_text(frankenstein_filepath))
     #print(f"Found {get_num_words(get_book_text(frankenstein_filepath))} total words.")
     #print(char_count)
+    #print(sort_char_count(char_count))
     
-    print(sort_char_count(char_count))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frankenstein_filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sort_char_count(char_count):
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
     
     
 main()
